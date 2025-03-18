4a6a97195810cdcf83b5f15525b89932085ac489
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2025-03-18 19:48:31 +0700

    finsh get sheet and write sheet

---
+++ node/repositories/mongodb.ts
@@ -0,0 +1,78 @@
+
+import { MongoClient, Db, Collection } from 'mongodb';
+// MongoDB connection URI
+const uri = 'mongodb://root:linhporo1@10.10.0.216:27019';
+const dbName = 'tele_bot_db';
+const runTimeChatIds: any = []
+let client: MongoClient;
+
+
+export async function connectToDatabase(): Promise<Db> {
+    if (!client) {
+        client = new MongoClient(uri, {
+            maxPoolSize: 10, // Set the connection pool size
+        });
+        await client.connect();
+        console.log('MongoDB connected with connection pooling enabled');
+    }
+
+    return client.db(dbName);
+}
+
+
+export async function createCollection(db: Db, collectionName: string) {
+    const collections = await db.listCollections().toArray();
+    if (collections.some((c: any) => c.name === collectionName)) {
+        console.log(`Collection ${collectionName} already exists`);
+        runTimeChatIds.push(collectionName)
+        return;
+    }
+    await db.createCollection(collectionName);
+    runTimeChatIds.push(collectionName)
+    console.log(`Collection ${collectionName} created`);
+}
+
+
+export async function insertDocument(db: Db, collectionName: string, document: any) {
+    if (!document) {
+        console.error('No document provided');
+        return;
+    }
+
+    try {
+        const collection: Collection = db.collection(collectionName);
+
+        // Insert the document into the collection
+        const result = await collection.insertOne(document);
+
+        if (result.acknowledged) {
+            console.log(`Document inserted with _id: ${result.insertedId}`);
+        } else {
+            console.error('Document insertion failed');
+        }
+    } catch (error) {
+        console.error('Error inserting document:', error);
+    }
+}
+
+export async function findOne(db: Db, collectionName: string, id: any) {
+
+    const collection: Collection = db.collection(collectionName);
+
+    // Find the document
+    const document = await collection.findOne({
+        id,
+    });
+    return document;
+}
+
+
+export async function queryDocuments(db: Db, collectionName: string, query: any) {
+    const collection: Collection = db.collection(collectionName);
+
+    // Find the documents
+    const documents = await collection.find(query).toArray();
+    return documents;
+}
+
+
